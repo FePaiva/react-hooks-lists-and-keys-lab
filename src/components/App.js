edec6a548d652c712858ad213e4e6bfc2d70de62
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2021-12-14 15:34:44 -0800

    Assignment complete

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,21 @@ import Home from "./Home";
 import About from "./About";
 import ProjectList from "./ProjectList";
 import user from "../data/user";
+import ProjectItem from "./ProjectItem";
 
 function App() {
+
+  // const projectCards = user.map((userObj)=> {
+  //   return <ProjectList key={userObj.id} name={userObj.name} about={userObj.about} technologies={userObj.technologies} />;
+  // })
+
   return (
     <div>
       <NavBar />
       <Home username={user.name} city={user.city} color={user.color} />
       <About bio={user.bio} links={user.links} />
       <ProjectList projects={user.projects} />
+      <ProjectItem /> 
     </div>
   );
 }
